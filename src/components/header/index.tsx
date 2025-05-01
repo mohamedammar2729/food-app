e031383cce92fcb3317ed2303f42f8c2415fcb2f
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-05-01 14:14:21 +0300

    Add header, navbar, link component, and enums for routing and user roles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,21 @@
-
+import { Routes } from "@/constants/enums";
+import Link from "../link";
+import NavBar from "./NavBar";
 
 function Header() {
-    return (
-      <div className="container">
-        <header>Hello</header>
+  return (
+    <header className="py-4 md:py-6">
+      <div className="container flex items-center justify-between ">
+        <Link
+          href={Routes.ROOT}
+          className="text-primary font-semibold text-2xl"
+        >
+          🍕 Food
+        </Link>
+        <NavBar />
       </div>
-    );
+    </header>
+  );
 }
 
 export default Header;
